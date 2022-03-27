8cf3a091c69271a1592bee0c23756122c54de57c
kristofermendoza <kris06mendoza@gmail.com>
2022-03-27 09:55:52 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import React, { useState } from 'react'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Books from './pages/Books'
-import { books } from "./data"
-import Bookinfo from './pages/BookInfo'
-import Cart from './pages/Cart'
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import Bookinfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    setCart([...cart, { ...book, quantity: 1 }])
+    setCart([...cart, { ...book, quantity: 1 }]);
   }
 
   function changeQuantity(book, quantity) {
-    setCart(cart.map((item) =>
-      item.id === book.id ?
-        {
-          ...item,
-          quantity: +quantity,
-        }
-        : item
-    )
-    )
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
   }
 
   function removeItem(item) {
-    setCart(cart.filter(book => book.id !== item.id))
+    setCart(cart.filter((book) => book.id !== item.id));
   }
 
   function numberOfItems() {
     let counter = 0;
-    cart.forEach(item => {
-      counter += item.quantity
-    })
-    return counter
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
   }
 
   return (
     <Router>
       <div className="App">
-        <Nav numberOfItems={numberOfItems()}/>
-        <Route path="/" exact component={Home} />
-        <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route path="/books/:id" render={() => <Bookinfo books={books} addToCart={addToCart} cart={cart} />} />
-        <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <Bookinfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                books={books}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
+        </Routes>
         <Footer />
       </div>
     </Router>
